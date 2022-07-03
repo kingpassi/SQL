6ed6e39edfa847a3a663b06662b82ffa8bff0107
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2022-07-03 21:21:15 +0530

    Add files via upload

---
+++ create table in sql.sql
@@ -0,0 +1,16 @@
+
+-- create
+ CREATE TABLE EMPLOYEE(
+emp_id int not null,
+first_name varchar(20),
+last_name varchar(20),
+salary int,
+primary key(emp_id)
+
+);
+
+
+-- insert
+
+-- fetch 
+describe EMPLOYEE ;
